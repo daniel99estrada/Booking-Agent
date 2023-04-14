f62236ca298704bed74c317b0f5c0fa186a2e4d9
Daniel Estrada <pildoestrada@gmail.com>
2023-04-14 17:41:38 -0400

    "added a Booking Agent Class"

--- City.java
+++ City.java
@@ -1,23 +1,12 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class City {
     private String name;
-    private List<Edge> edges;
     
     public City(String name) {
         this.name = name;
-        edges = new ArrayList<>();
     }
     
     // Getter and setter for name attribute
     public String getName() {
         return name;
-    }
-    
-    // Method to add an Edge to the list
-    public void addEdge(City destination, float cost) {
-        Edge edge = new Edge(this, destination, cost);
-        edges.add(edge);
     }
 }
---
+++ Graph.java
@@ -0,0 +1,8 @@
+import java.utils.List;
+import java.utils.ArrayList;
+import java.utils.List;
+
+public class Graph {
+    public List<Edge> flights;
+
+}
